6e971a74c8ab4f0d31fb9ab04e1a824291bfd13c
Charles Chen <chen.charles.c@gmail.com>
2022-10-15 17:03:31 -0400

    Added log message

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ app.Urls.Add("http://0.0.0.0:8080");
 
 app.MapGet("/", () =>
 {
+  Console.WriteLine("THIS IS A LOG MESSAGE!!!");
+
   return $"Hello, World @ {DateTime.UtcNow} - version 3!!!";
 })
 .WithName("hello");
